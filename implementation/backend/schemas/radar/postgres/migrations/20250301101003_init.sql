82ae4ba486488ef9ceade082073d84aacee3b6a7
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2025-03-02 18:01:40 +0700

    add local db docker config

---
+++ implementation/backend/schemas/radar/postgres/migrations/20250301101003_init.sql
@@ -0,0 +1,135 @@
+-- migrate:up
+CREATE TYPE area_type as ENUM ('numbered-seating', 'free-standing');
+CREATE TYPE seat_status as ENUM ('available', 'on-hold', 'sold');
+CREATE TYPE order_status as ENUM ('waiting-for-payment', 'failed', 'success');
+CREATE TYPE invoice_status as ENUM ('pending', 'expired', 'paid');
+
+CREATE TABLE "events" (
+    id bigserial primary key,
+    name text not null,
+    location text not null,
+    description text not null,
+
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
+);
+
+CREATE TABLE "ticket_categories" (
+    id bigserial primary key,
+    name text not null,
+
+    event_id bigint not null references events(id) on update cascade on delete cascade,
+
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
+);
+
+CREATE TABLE "ticket_sales" (
+    id bigserial primary key,
+    name text not null,
+    sale_begin_at timestamptz not null,
+    sale_end_at timestamptz not null,
+
+    event_id bigint not null references events(id) on update cascade on delete cascade,
+
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
+);
+
+CREATE TABLE "ticket_packages" (
+    id bigserial primary key,
+    stock int not null,
+    price int not null,
+
+    ticket_category_id bigint not null references ticket_categories(id) on update cascade on delete cascade,
+    ticket_sale_id bigint not null references ticket_sales(id) on update cascade on delete cascade,
+
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
+);
+
+CREATE TABLE "ticket_areas" (
+    id bigserial primary key,
+    type area_type not null,
+
+    ticket_package_id  bigint not null references ticket_packages(id) on update cascade on delete cascade,
+
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
+);
+
+CREATE TABLE "ticket_seats" (
+    id bigserial primary key,
+    seat_number text not null,
+    status seat_status not null default 'available',
+
+    ticket_area_id bigint not null references ticket_areas(id) on update cascade on delete cascade,
+
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
+);
+
+CREATE TABLE "orders" (
+    id bigserial primary key,
+    status order_status not null,
+    fail_reason text,
+
+    external_user_id text not null,
+    invoice_id bigint not null references invoices(id) on update cascade on delete cascade,
+
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
+);
+
+CREATE TABLE "order_items" (
+    id bigserial primary key,
+    customer_name text not null,
+    customer_email text not null,
+    price int not null,
+
+    order_id bigint not null references orders(id) on update cascade on delete cascade,
+
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
+);
+
+CREATE TABLE "invoices"
+(
+    id     bigserial primary key,
+    status invoice_status not null default 'pending',
+    amount int not null,
+    external_id text not null,
+
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
+);
+
+CREATE TABLE "issued_tickets" (
+    id bigserial primary key,
+    serial_number text not null,
+    holder_name text not null,
+
+    seat_id bigint not null references ticket_seats(id) on update cascade on delete cascade,
+    order_id bigint not null references orders(id) on update cascade on delete cascade,
+    order_item_id bigint not null references order_items(id) on update cascade on delete cascade,
+
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
+);
+
+-- migrate:down
+
+DROP TABLE "issued_tickets";
+DROP TABLE "invoices";
+DROP TABLE "order_items";
+DROP TABLE "orders";
+DROP TABLE "ticket_seats";
+DROP TABLE "ticket_areas";
+DROP TABLE "ticket_packages";
+DROP TABLE "ticket_sales";
+DROP TABLE "ticket_categories";
+DROP TABLE "events";
+DROP TYPE invoice_status;
+DROP TYPE order_status;
+DROP TYPE seat_status;
+DROP TYPE area_type;
